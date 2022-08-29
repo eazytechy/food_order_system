2f35b14fc9ef40c529716b0ea8dd813632802ec6
Sumit Jain <sumit@eazytechy.com>
2022-08-30 01:26:00 +0530

    added requirements.txt, config, static & apps folder

---
+++ config/development.py
@@ -0,0 +1,28 @@
+from config.common import *
+import os
+
+ENV = "DEVELOPMENT"
+DEBUG = True
+
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dev_database',
+        'USER': 'et_dev_user',
+        'PASSWORD': 'et@user@123',
+        "HOST": "127.0.0.1",
+        'TIME_ZONE': 'Asia/Kolkata'
+    }
+}
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
+
--- food_order_system/wsgi.py
+++ food_order_system/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for foodsystem project.
+WSGI config for food order system project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foodsystem.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.development')
 
 application = get_wsgi_application()
